02b6849cff059b4bd53e3a0ec30abd4a1798ca25
Medugu <meduguisaac07@gmail.com>
2024-03-12 11:57:14 +0100

    My travel Advisor

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import {paper, Typography, useMediaQuery} from '@material-ui/core'
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
+import Rating from '@material-ui/lab'
+
+import useStyles from './MapStyles'
+
+const Map = ({setCoordinates,setBounds,coordinates}) => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+    const apiKey = process.env.REACT_APP_API_KEY;
+    return(
+        <div className={classes.mapContainer}>
+            <GoogleMapReact 
+            bootstrapURLkeys={{key: apiKey}}
+            defaultCenter = {coordinates}
+            center={coordinates}
+            defaultZoom={14}
+            margin={[50,50,50,50]}
+            options = {''}
+            onChange = {(e)=>{
+                console.log(e)
+                setCoordinates({lat: e.center.lat, lng: e.center.lng});
+                setBounds({ne: e.marginBounds.ne, sw: e.marginBounds.sw})
+            }}
+            onChildClick={ ''}
+            >
+               
+            </GoogleMapReact>
+        </div>
+    )
+}
+
+export default Map;
---
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PlaceDetails = (props) => {
+    return(
+        <h1>{props.name}</h1>
+    )
+}
+
+export default PlaceDetails;
